e99b4a074c8be1c3d1ea82557152f6a37395be3f
breno.minzon@gmail.com <Milen@bren0>
2021-08-23 18:18:35 -0400

    included percentage of processing

--- th2sensorcsvparser.py
+++ th2sensorcsvparser.py
@@ -2,16 +2,28 @@ import csv
 import dblib
 
 lines = []
+
+
+def percentage(part, whole):
+    percent = 100 * float(part)/float(whole)
+    return "%.2f" % percent + "%"
+
+
 with open('door.csv', 'r') as mycsv:
     reader = csv.DictReader((x.replace('\0', '').replace('ÿþ', '') for x in mycsv), delimiter='\t')
+    readercopy = reader
     rowwritten = 0
     rowskipped = 0
-    print("Writting in db. Please wait...")
-    for row in reader:
-        # print(row)
+    rows = list(reader)
+    totalrows = len(rows)
+    for i, row in enumerate(rows):
         if dblib.saveData(row['Time'], row['Temperature(C)'], row['Humidity(%RH)']):
             rowwritten = rowwritten + 1
         else:
             rowskipped = rowskipped + 1
+        if i + 1 == totalrows:
+            print("Writting in db. Please wait...%s" % percentage(i+1, totalrows) + " Done!")
+        else:
+            print("Writting in db. Please wait...%s" % percentage(i+1, totalrows), end='\r')
     print("" + str(rowwritten) + " rows written in db")
     print("" + str(rowskipped) + " rows skipped")
